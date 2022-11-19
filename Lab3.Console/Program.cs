922c7d06da9837d77342c3295399f0a32e38e3ea
qwert <qwert@ARANOD>
2022-11-19 11:02:53 +0200

    Add project files.

---
+++ Lab3.Console/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Lab3.Hash;
+
+var hashFunction2bit = new HashFunction(2);
+var hashFunction4bit = new HashFunction(4);
+var hashFunction8bit = new HashFunction(8);
---
+++ Lab3.Hash/HashFunction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3.Hash
+{
+    public class HashFunction
+    {
+        private readonly byte _resultSize;
+
+        public HashFunction(byte resultSize)
+        {
+            _resultSize = resultSize;
+        }
+
+        public string Calculate(string message)
+        {
+            return null;
+        }
+    }
+}
